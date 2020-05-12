3f2a9008e6b1f33c178b8685d516b974100eb315
IAmRDhar <rahul.dhar47@gmail.com>
2020-05-12 22:09:18 +0530

    Made modifications such that the html and images url can be given from command line

--- index.js
+++ index.js
@@ -1,51 +1,80 @@
 var fs = require("fs");
 var webpage = require("webpage");
-var loadInProgress = false;
+var system = require('system');
 
-var page = webpage.create();
-var htmlFiles = new Array();
-console.log("Working directory: ", fs.workingDirectory);
-var curdir = fs.list(fs.workingDirectory);
+var curdir;
+var htmlDirectory = fs.workingDirectory + "/html";
+var imageDirectory = fs.workingDirectory = "/images"
 
-// loop through files and folders
-for (var i = 0; i < curdir.length; i++) {
-  var fullpath = fs.workingDirectory + fs.separator + curdir[i];
-  // check if item is a file
-  console.log("Full path", fullpath);
-  if (fs.isFile(fullpath)) {
-    // check that file is html
-    if (fullpath.indexOf(".html") != -1) {
-      // show full path of file
-      console.log("File path: " + fullpath);
-      htmlFiles.push(fullpath);
-    }
-  }
-}
+setWorkingDirectories(system.args);
+deleteImageDirectory(imageDirectory);
 
+var htmlFiles = readAllHTMLFiles();
 console.log("Number of Html Files: " + htmlFiles.length);
 
+var loadInProgress = false;
 // output pages as PNG
 var pageindex = 0;
-
+var page = webpage.create();
 var interval = setInterval(function () {
   if (!loadInProgress && pageindex < htmlFiles.length) {
-    console.log("image " + (pageindex + 1));
+    console.log("Image " + (pageindex + 1));
+    console.log(htmlFiles[pageindex]);
     page.open(htmlFiles[pageindex]);
   }
   if (pageindex == htmlFiles.length) {
-    console.log("image render complete!");
+    console.log("Image render complete!");
     phantom.exit();
   }
 }, 250);
 
 page.onLoadStarted = function () {
   loadInProgress = true;
-  console.log("page " + (pageindex + 1) + " load started");
+  console.log("Page " + (pageindex + 1) + " load started");
 };
 
 page.onLoadFinished = function () {
   loadInProgress = false;
-  page.render("images/output" + (pageindex + 1) + ".png");
-  console.log("page " + (pageindex + 1) + " load finished");
+  var pngName = htmlFiles[pageindex].match(/\/([^\/]+)\.html/);
+  console.log("Name", pngName[1]);
+  page.render(imageDirectory + "/" + pngName[1] + ".png");
+  console.log("Page " + (pageindex + 1) + " load finished");
   pageindex++;
 };
+
+function setWorkingDirectories (args){
+  if (args.length === 1) {
+    console.log('No arguments found, taking current directory as the working directory.');
+    curdir = fs.list(workingDirectory);
+  } else if(args.length === 2 ) {
+    htmlDirectory = args[1];
+    curdir = fs.list(args[1]);
+  } else {
+    htmlDirectory = args[1];
+    curdir = fs.list(args[1]);
+    imageDirectory = args[2];
+  }
+}
+
+function deleteImageDirectory(toDelete) {
+  fs.removeTree(toDelete);
+}
+
+function readAllHTMLFiles(){
+  var htmlFiles = new Array();
+  // loop through files and folders
+  for (var i = 0; i < curdir.length; i++) {
+    var fullpath = htmlDirectory + fs.separator + curdir[i];
+    // check if item is a file
+    console.log("Full path", fullpath);
+    if (fs.isFile(fullpath)) {
+      // check that file is html
+      if (fullpath.indexOf(".html") != -1) {
+        // show full path of file
+        console.log("File path: " + fullpath);
+        htmlFiles.push(fullpath);
+      }
+    }
+  }
+  return htmlFiles;
+}
